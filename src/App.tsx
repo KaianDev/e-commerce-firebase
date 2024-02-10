6ca5e781c7398ebebac375a126c863a3cab1f382
Kaian Vasconcelos <kaianvasconcelos@gmail.com>
2024-02-09 22:22:59 -0300

    feat: add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 //Components
-import Header from './components/Header'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+// Pages
+import HomePage from './pages/HomePage'
 
 const App = () => {
   return (
-    <>
-      <Header />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
