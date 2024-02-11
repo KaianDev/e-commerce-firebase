2cc2ccc6bf85eb04e9ce7387091429bf90acd624
Kaian Vasconcelos <kaianvasconcelos@gmail.com>
2024-02-11 15:46:21 -0300

    feat: add Custom Input Component

---
+++ src/components/CustomInput/index.tsx
@@ -0,0 +1,20 @@
+import { ComponentProps, forwardRef } from 'react'
+
+// Styles
+import * as C from './styles'
+
+interface CustomInputProps extends ComponentProps<'input'> {
+  hasError?: boolean
+}
+
+type Ref = HTMLInputElement
+
+const CustomInput = forwardRef<Ref, CustomInputProps>(
+  ({ hasError, ...rest }, ref) => (
+    <C.Input ref={ref} error={hasError} {...rest} />
+  ),
+)
+
+CustomInput.displayName = 'CustomInput'
+
+export default CustomInput
