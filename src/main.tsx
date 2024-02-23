3bf620e7b397d91fbb728da4f979efbbef50a92f
Kaian Vasconcelos <kaianvasconcelos@gmail.com>
2024-02-22 23:26:17 -0300

    feat: add redux-persist for persist state in local storage

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
@@ -6,14 +7,18 @@ import './index.css'
 // Utilities
 import Providers from './providers/providers.tsx'
 import { Provider } from 'react-redux'
-import store from './store/store.ts'
+import { store, persistedStore } from './store/store.ts'
+// @ts-ignore
+import { PersistGate } from 'redux-persist/es/integration/react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <Providers>
-        <App />
-      </Providers>
+      <PersistGate persistor={persistedStore}>
+        <Providers>
+          <App />
+        </Providers>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 )
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,23 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { applyMiddleware, createStore } from 'redux'
-import rootReducer from './rootReducer'
 import logger from 'redux-logger'
+import rootReducer from './rootReducer'
+// @ts-ignore
+import storage from 'redux-persist/lib/storage'
+// @ts-ignore
+import persistReducer from 'redux-persist/es/persistReducer'
+// @ts-ignore
+import persistStore from 'redux-persist/es/persistStore'
 
-const store = createStore(rootReducer, applyMiddleware(logger))
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cartReducer'],
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+export const store = createStore(persistedReducer, applyMiddleware(logger))
+export const persistedStore = persistStore(store)
 
 export type AppDispatch = typeof store.dispatch
 export default store
